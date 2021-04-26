b27140863a3964477afefcf3b157302eeccf93b5
aith <ariiramanesh@gmail.com>
2021-04-26 15:04:15 -0700

    atomicized filter

--- part1/filter.h
+++ part1/filter.h
@@ -13,16 +13,16 @@ class mutex {
   }
   
   void init (int num_threads) {
-    n = num_threads;
+    n.store(num_threads);
     for (int i = 0; i < num_threads; i++) {
-        levels[i] = 0;
-        victims[i] = 0;
+        levels[i].store(0);
+        victims[i].store(0);
     }
   }
 
 bool find_threads_at_gte_level(int thread_id) {
     for (int i=0; i< n; i++) {
-        if (levels[i] >= levels[thread_id] && i != thread_id) { return true; }
+        if (levels[i].load() >= levels[thread_id].load() && i != thread_id) { return true; }
     }
     return false;
 }
@@ -30,19 +30,19 @@ bool find_threads_at_gte_level(int thread_id) {
   void lock(int thread_id) {
     int me = thread_id;
     for (int i = 1; i < n; i++) {
-        levels[me] = i;
-        victims[i] = me;
+        levels[me].store(i);
+        victims[i].store(me);
         while(find_threads_at_gte_level(thread_id)  /*  there are threads with higher levels */
-        && victims[i] == me) { std::this_thread::yield(); }
+        && victims[i].load() == me) { std::this_thread::yield(); }
     }
   }
 
   void unlock(int thread_id) {
-    levels[thread_id] = 0;
+    levels[thread_id].store(0);
   }
 
  private:
-  int levels[MAX_THREADS];  // indexed by the thread id, whose element is their level
-  int victims[MAX_THREADS];
-  int n;
+  atomic_int levels[MAX_THREADS];  // indexed by the thread id, whose element is their level
+  atomic_int victims[MAX_THREADS];
+  atomic_int n;
 };
