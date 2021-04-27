34d6444d762ede3112f75c8d6a2186c49ea8e0af
aith <ariiramanesh@gmail.com>
2021-04-27 12:42:39 -0700

    added lock to coarse lock

--- part3/coarse_lock_stack.h
+++ part3/coarse_lock_stack.h
@@ -1,78 +1,93 @@
 #include <mutex>
+
 using namespace std;
 
 class Llist_node {
- public:  
-  Llist_node(int d) {   
-    data = d;
-    next = NULL;
-  }
-  
-  int data;
-  Llist_node *next;
+public:
+    Llist_node(int d) {
+        data = d;
+        next = NULL;
+    }
+
+    int data;
+    Llist_node *next;
 };
 
 class CSE113_Stack {
- public:
-  
-  CSE113_Stack() {
-    start = NULL;
-  }
-  
-  int pop() {
-    if (start == NULL) {
-      return -1;
+public:
+
+    CSE113_Stack() {
+        start = NULL;
     }
 
-    if (start->next == NULL) {
-      int ret = start->data;
-      delete start;
-      start = NULL;
-      return ret;
+    int pop() {
+        lock.lock();
+
+        if (start == NULL) {
+            lock.unlock();
+            return -1;
+        }
+
+        if (start->next == NULL) {
+            int ret = start->data;
+            delete start;
+            start = NULL;
+            lock.unlock();
+            return ret;
+        }
+
+        Llist_node *current = start->next;
+        Llist_node *previous = start;
+        while (current->next != NULL) {
+            previous = current;
+            current = current->next;
+        }
+
+        int ret = current->data;
+        previous->next = NULL;
+        delete current;
+
+        lock.unlock();
+
+        return ret;
     }
 
-    Llist_node *current = start->next;
-    Llist_node *previous = start;
-    while (current->next != NULL) {
-      previous = current;
-      current = current->next;
+
+    int peek() {
+        lock.lock();
+        if (start == NULL) {
+            lock.unlock();
+            return -1;
+        }
+
+        Llist_node *current = start;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        lock.unlock();
+        return current->data;
     }
 
-    int ret = current->data;
-    previous->next = NULL;
-    delete current;
-    return ret;
-  }
+    void push(int p) {
+        lock.lock();
 
-  
-  int peek() {
-    if (start == NULL) {
-      return -1;
-    }
-    
-    Llist_node *current = start;
-    while (current->next != NULL) {
-      current = current->next;
-    }
-    return current->data;    
-  }
-  
-  void push(int p) {
-    if (start == NULL) {
+        if (start == NULL) {
 
-      start = new Llist_node(p);
-      return;
+            start = new Llist_node(p);
+            lock.unlock();
+            return;
+        }
+        Llist_node *current = start;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new Llist_node(p);
+
+        lock.unlock();
     }
 
-    Llist_node *current = start;
-    while (current->next != NULL) {
-      current = current->next;
-    }
-
-    current->next = new Llist_node(p);
-  }
-
- private:
-  Llist_node *start;  
+private:
+    Llist_node *start;
+    mutex lock;
 };
 
