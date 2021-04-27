43062f82a7075186e7c0d0ef723c12dc264c4fcc
aith <ariiramanesh@gmail.com>
2021-04-26 21:22:16 -0700

    added more fair reader-writer lock based

--- part2/fair_mutex.h
+++ part2/fair_mutex.h
@@ -2,31 +2,61 @@
 
 #include <atomic>
 #include <mutex>
+#include <semaphore.h>
 using namespace std;
 
 class rw_mutex {
- public:
-  rw_mutex() {
-    // Implement me!
-  }
+public:
+    rw_mutex() {
+        sem_i = sem_init(&wrt, 0, 0);
+        ctrin.store(0);
+        ctrout.store(0);
+        wait.store(0);
+    }
 
-  void lock_reader() {
-    // Implement me!
-  }
-  
-  void unlock_reader() {
-    // Implement me!
-  }
+    ~rw_mutex() {
+        sem_destroy(&wrt);
+    }
 
-  
-  void lock() {
-    // Implement me!
-  }
-  
-  void unlock() {
-    // Implement me!
-  }
+    void lock_reader() {
+        in.lock();
+        atomic_fetch_add(&ctrin, 1);
+        in.unlock();
+    }
 
- private:
-  // Give me some private variables!
+    void unlock_reader() {
+        out.lock();
+        atomic_fetch_add(&ctrout, 1);
+        if (wait.load() == 1 && ctrin.load() == ctrout.load()) {
+            sem_post(&wrt);
+        }
+        out.unlock();
+    }
+
+    void lock() { /* Writer lock*/
+        in.lock();
+        out.lock();
+        if (ctrin.load() == ctrout.load()) {
+            out.unlock();
+        }
+        else {
+            wait.store(1);
+            out.unlock();
+            sem_wait(&wrt);
+            wait.store(0);
+        }
+    }
+
+    void unlock() {
+        in.unlock();
+    }
+
+private:
+    mutex in;
+    mutex out;
+    sem_t wrt;
+    int sem_i;
+    atomic_int ctrin;
+    atomic_int ctrout;
+    atomic_bool wait;
 };
